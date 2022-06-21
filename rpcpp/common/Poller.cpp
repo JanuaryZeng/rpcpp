2d3391836eaf520b92926d0ecf71342823742cb7
januaryzeng <15841331413@163.com>
2022-06-21 20:21:28 +0800

    poller、rpcException

---
+++ rpcpp/common/Poller.cpp
@@ -0,0 +1,55 @@
+#include "Poller.h"
+
+using namespace rpcpp;
+Poller::Poller(int max_event_number):max_event_number(max_event_number)
+{
+    events_ = new epoll_event[max_event_number];
+    epollfd = epoll_create(5);
+}
+
+Poller::~Poller()
+{
+    delete[] events_;
+}
+int Poller::addfd(int fd, bool one_shot) {
+    epoll_event event;
+    event.data.fd = fd;
+
+    event.events = EPOLLIN | EPOLLRDHUP;
+
+    if (one_shot)
+        event.events |= EPOLLONESHOT;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    return setnonblocking(fd);
+}
+int Poller::removefd(int fd) {
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    return close(fd);
+}
+int Poller::wait(std::vector<epoll_event> &events,int timeout) {
+    int number = epoll_wait(epollfd, events_, max_event_number, timeout);
+    if(number<0)
+        return number;
+    for(int i=0;i<number;i++)
+    {
+        events.push_back(events_[i]);
+    }
+    return number;
+}
+
+int Poller::setnonblocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL, 0);
+    int new_option = old_option | O_NONBLOCK;
+    if(fcntl(fd, F_SETFL, new_option)!=0){
+        return -1;
+    }
+    return old_option;
+}
+
+int Poller::modfd(int fd, int ev)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+    return epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+}
---
+++ rpcpp/common/RpcException.cpp
@@ -0,0 +1,61 @@
+
+
+#include "rpcpp/common/RpcException.h"
+
+using namespace rpcpp;
+
+RpcException::RpcException(int code) : code(code), message(Errors::GetErrorMessage(code))
+{
+    setWhatMessage();
+}
+
+RpcException::RpcException(int code, const std::string &message) : code(code), message(Errors::GetErrorMessage(code))
+{
+    if (!this->message.empty())
+        this->message = this->message + ": ";
+    this->message = this->message + message;
+    setWhatMessage();
+}
+
+RpcException::RpcException(int code, const std::string &message, const Json::Value &data)
+    : code(code), message(Errors::GetErrorMessage(code)), data(data)
+{
+    if (!this->message.empty())
+        this->message = this->message + ": ";
+    this->message = this->message + message;
+    setWhatMessage();
+}
+
+RpcException::RpcException(const std::string &message) : code(0), message(message)
+{
+    setWhatMessage();
+}
+
+RpcException::~RpcException() throw() {}
+
+int RpcException::GetCode() const { return code; }
+
+const std::string &RpcException::GetMessage() const { return message; }
+
+const Json::Value &RpcException::GetData() const { return data; }
+
+const char *RpcException::what() const throw()
+{
+    return whatString.c_str();
+}
+
+void RpcException::setWhatMessage()
+{
+    if (code != 0)
+    {
+        std::stringstream ss;
+        ss << "Exception " << code << " : " << message;
+        if (data != Json::nullValue)
+            ss << ", data: " << data.toStyledString();
+        whatString = ss.str();
+    }
+    else
+    {
+        whatString = message;
+    }
+}
